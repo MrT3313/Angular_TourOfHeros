cd89e9258d944274c028cac4aa4f37a72ffc2662
MrT3313 <turgeon.dev@gmail.com>
2020-10-17 21:00:03 -0400

    Displaying a selectable list of heros. On selection individual hero information is populated with 2 way bound editing input

--- client/src/app/heroes/heroes.component.ts
+++ client/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from './hero.interface'
+import { MOCK_HEROES } from '../../__mocks__/heroes.mock'
 
 @Component({
   selector: 'app-heroes',
@@ -9,14 +10,16 @@ import { Hero } from './hero.interface'
 })
 
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
-
+  heroes = MOCK_HEROES;
+  selectedHero: Hero;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero
   }
 
 }
