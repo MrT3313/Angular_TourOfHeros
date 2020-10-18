26e3e1b80e354d7749b157d91f831d7319452a30
MrT3313 <turgeon.dev@gmail.com>
2020-10-18 01:32:34 -0400

    Initial Routing

---
+++ client/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../../interface/hero.interface'
+import { HeroesDataService } from '../../services/heroesData.service'
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.less']
+})
+
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
+
+  constructor(private heroesDataSerivice: HeroesDataService) { }
+
+  ngOnInit(): void {
+    this.getHeroes()
+  }
+
+  getHeroes(): void {
+    this.heroesDataSerivice.getHeroes()
+      .subscribe(heroes => this.heroes = heroes.slice(1,5))
+  }
+
+}
